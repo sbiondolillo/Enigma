5c138bd9cabe5accaefb5435fbcfb5321205c6aa
Samuel Biondolillo <biwinning121@gmail.com>
2017-09-23 11:47:36 -0400

    Create Output and FileOutput Interfaces

---
+++ src/interfaces/Output.java
@@ -0,0 +1,17 @@
+/*
+ * Output Interface
+ * Samuel Biondolillo
+ * CIS220M:HY1 Object Oriented Programming
+ * Goal: Create a base interface for output processing in the Enigma machine
+ * Version 0.0.1 - 9/23/17
+ */
+
+package interfaces;
+
+public interface Output {
+
+	/*
+	 * Getter for instance variable messageOut
+	 */
+	String getMessageOut();
+}
