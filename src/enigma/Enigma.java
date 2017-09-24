d54ceb56a408b55710cede3dc89dbcd3c97af842
Samuel Biondolillo <biwinning121@gmail.com>
2017-09-24 17:31:09 -0400

    Adjust Enigma to utilize three rotors

--- src/enigma/Enigma.java
+++ src/enigma/Enigma.java
@@ -6,6 +6,7 @@
  * Version 0.0.1 - 9/18/17
  * 		   0.0.2 - 9/23/17	Add logic to user input to scale to size of Rotor dictionary
  * 							refactor user input to include correct methods from InputProcessor
+ * 		   0.0.3 - 9/24/17	Adjust Enigma to utilize three rotors in series
  */
 
 package enigma;
@@ -19,6 +20,8 @@ public class Enigma implements EnigmaApparatus {
 	private int offset;
 	private InputProcessor mainIP;
 	private Rotor rotor1 = new Rotor();
+	private Rotor rotor2 = new Rotor();
+	private Rotor rotor3 = new Rotor();
 	private Scanner inputScanner = new Scanner(System.in);
 	
 	/*
@@ -31,7 +34,7 @@ public class Enigma implements EnigmaApparatus {
 		
 		// Get the initial rotor setting from the user
 		while (true) {
-			System.out.println("Please enter an initial setting for your rotor from 0 to " + 
+			System.out.println("Please enter an initial setting for the first rotor from 0 to " + 
 					(rotor1.getValidCharacters().length() - 1) + ": ");
 			offset = inputScanner.nextInt();
 			if (offset < 0 || offset > rotor1.getValidCharacters().length()-1) {
@@ -39,6 +42,30 @@ public class Enigma implements EnigmaApparatus {
 			}
 			else {
 				rotor1.setIndex(offset);
+				break;
+			}
+		}
+		while (true) {
+			System.out.println("Please enter an initial setting for the second rotor from 0 to " + 
+					(rotor2.getValidCharacters().length() - 1) + ": ");
+			offset = inputScanner.nextInt();
+			if (offset < 0 || offset > rotor2.getValidCharacters().length()-1) {
+				continue;
+			}
+			else {
+				rotor2.setIndex(offset);
+				break;
+			}
+		}
+		while (true) {
+			System.out.println("Please enter an initial setting for the third rotor from 0 to " + 
+					(rotor3.getValidCharacters().length() - 1) + ": ");
+			offset = inputScanner.nextInt();
+			if (offset < 0 || offset > rotor3.getValidCharacters().length()-1) {
+				continue;
+			}
+			else {
+				rotor3.setIndex(offset);
 				break;
 			}
 		}
@@ -112,12 +139,12 @@ public class Enigma implements EnigmaApparatus {
 		// TODO - break this into parts, move into appropriate methods
 		// TODO - add logic to use OutputProcessor to publish results
 		
-		// Encode the message using the rotor, output cyphertext
+		// Encode the message using the rotors, output cyphertext
 		System.out.println("Your original message:" + mainIP.getMessageIn());
 		System.out.println("Encoding...");
 		System.out.print("Your encoded message: ");
 		for (int i = 0; i < mainIP.getMessageIn().length(); i++) {
-			System.out.print(rotor1.encode(mainIP.getMessageIn().charAt(i)));
+			System.out.print(rotor3.encode(rotor2.encode(rotor1.encode(mainIP.getMessageIn().charAt(i)))));
 		}
 		inputScanner.close();
 	}
