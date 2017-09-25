9efbb310d36fcbea2093e97e06a3caeb3d96fa06
Samuel Biondolillo <biwinning121@gmail.com>
2017-09-24 23:24:08 -0400

    update RotaryEncryptor to include notch variable

--- src/interfaces/RotaryEncryptor.java
+++ src/interfaces/RotaryEncryptor.java
@@ -5,6 +5,7 @@
  * Goal: Create an interface for emulating a mechanical rotor in the Enigma program
  * Version 0.0.1 - 9/22/17
  * 		   0.0.2 - 9/23/17	Refactored to make this an extension of Ecryptor
+ * 		   0.0.3 - 9/24/17  Added the getter for instance variable notch
  */
 
 package interfaces;
@@ -12,7 +13,8 @@ package interfaces;
 public interface RotaryEncryptor extends Encryptor {
 
 	/*
-	 * Getters and setters for instance variables
+	 * Getters and setters for instance variables index and notch
+	 * 		notch is only set in the constructor, never at runtime
 	 */
 	int getIndex();
 	void setIndex(int index);
@@ -20,7 +22,7 @@ public interface RotaryEncryptor extends Encryptor {
 	
 	/* 
 	 * Uses the current index to encode a single character.
-	 * Returns the encoded character or '#' if the encoding cannot be completed
+	 * @param plaintext - any char from validCharacters
 	 */
 	Character encode(Character plaintext);
 }
