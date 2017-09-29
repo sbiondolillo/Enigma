46bf1564f2ecf9e9a08a5761b92fc0ad4f43941b
Samuel Biondolillo <biwinning121@gmail.com>
2017-09-29 17:59:31 -0400

    build Utilities outline

--- src/utilities/Utilities.java
+++ src/utilities/Utilities.java
@@ -8,6 +8,58 @@
 
 package utilities;
 
-public class Utilities {
+public class Utilities {        
+	
+	/*
+	 * Makes a call to a method in a class within the utilities package
+	 * @param code - an int provided by the user from an input prompt
+	 */
+    public void load(int code){
+        try{
+            switch (code) {
+            // TODO - build up this menu interface with method calls based on code
+                case 0: 
+                    break;
+                    
+                case 1:
+                    break;
+                    
+                case 2:
+                    break;
+                          
+                case 3:
+                    break;
+                    
+                case 4:
+                    break;
+                              
+                case 5:
+                    break;
+                          
+                case 6:
+                    break;
+                    
+                case 7:
+                    break;
+                    
+                case 8:
+                    break;
+                          
+                case 9:
+                    break;
+                    
+                case 10:
+                    break;
+                              
+                case 11:
+                    break;
+                    
+                default:
+                    System.out.println("Sorry, that is not a vaild code");
+                    break;
+            }
+        }
+        catch(Exception NA){}
 
+    }
 }
