f9f7197cf38b980be3f477d50da1c859c97abe86
Samuel Biondolillo <biwinning121@gmail.com>
2017-09-29 19:01:25 -0400

    build Screens outline

--- src/utilities/Screens.java
+++ src/utilities/Screens.java
@@ -9,5 +9,55 @@
 package utilities;
 
 class Screens {
+	
+	/*
+	 * Display the initial prompt to the user when the program launches
+	 */
+	void displayWelcomeScreen() {
+		// TODO implement this
+	}
+	
+	/*
+	 * Display the screen where the user sets the machine's input
+	 */
+	void displayInputScreen() {
+		// TODO implement this
+	}
+	
+	/*
+	 * Display the screen where the user set's the machine's output
+	 */
+	void displayOutputScreen() {
+		// TODO implement this
+	}
+	
+	/*
+	 * Display the screen where the user can receive help
+	 */
+	void displayHelpScreen() {
+		// TODO - implement this
+	}
+	
+	/*
+	 * Display the screen where users can get general info about the program
+	 */
+	void displayAboutScreen() {
+		// TODO implement this
+	}
+	
+	/*
+	 * Display the screen where the user can view configuration info - no mods
+	 */
+	void displayConfigScreen() {
+		// TODO implement this
+	}
+	
+	/*
+	 * Display the final screen before the program exits
+	 */
+	void displayExitScreen() {
+		// TODO implement this
+	}
+	
 
 }
