6ca26e99a0f4c1632c4aa50735442c092ff9456a
Samuel Biondolillo <biwinning121@gmail.com>
2017-10-04 14:06:12 -0400

    build encode() method in RotorManager

--- src/rotors/RotorController.java
+++ src/rotors/RotorController.java
@@ -38,8 +38,11 @@ public class RotorController implements RotationManager{
 	 */
 	@Override
 	public String encode(String plaintext) {
-		// TODO Auto-generated method stub
-		return null;
+		String output = "";
+		for (char c: plaintext.toCharArray()) {
+			output += activeRotors[4].encode(activeRotors[3].encode(activeRotors[2].encode(activeRotors[1].encode(activeRotors[0].encode(c)))));
+		}
+		return output;
 	}
 	
 	/*
