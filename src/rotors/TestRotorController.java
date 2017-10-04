c3891898cc9bd2c8443531ce7d8d0bfba7abebac
Samuel Biondolillo <biwinning121@gmail.com>
2017-10-04 15:45:12 -0400

    update test class to include decode()

--- src/rotors/TestRotorController.java
+++ src/rotors/TestRotorController.java
@@ -8,11 +8,23 @@ public class TestRotorController {
 		
 		Scanner input = new Scanner(System.in);
 		RotorController rc = new RotorController();
-		System.out.print("Please enter a string to encode: ");
-		String plaintext = input.nextLine();
-		String cyphertext = rc.encode(plaintext);
-		System.out.println("Plaintext : " + plaintext);
-		System.out.println("Cyphertext: " + cyphertext);
+		System.out.print("Please enter 1 to encode or 2 to decode: ");
+		int mode = input.nextInt();
+		input.nextLine();
+		if (mode == 1) {
+			System.out.println("Please enter the text to encode: ");
+			String plaintext = input.nextLine();
+			String cyphertext = rc.encode(plaintext);
+			System.out.println("Plaintext : " + plaintext);
+			System.out.println("Cyphertext: " + cyphertext);
+		}
+		else if (mode == 2) {
+			System.out.println("Please enter the text to decode: ");
+			String cyphertext = input.nextLine();
+			String plaintext = rc.decode(cyphertext);
+			System.out.println("Cyphertext: " + cyphertext);
+			System.out.println("Plaintext : " + plaintext);
+		}
 		input.close();
 	}
 
