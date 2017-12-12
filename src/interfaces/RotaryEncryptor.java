f056b80b6d75adcb4b6e1af928f1d454af1a3f73
Sam Biondolillo <biwinning121@gmail.com>
2017-12-12 10:00:29 -0500

    Update RotaryEncryptor.java

--- src/interfaces/RotaryEncryptor.java
+++ src/interfaces/RotaryEncryptor.java
@@ -1,13 +1,3 @@
-/*
- * RotaryEncryptor Interface
- * Samuel Biondolillo
- * CIS220M:HY1 Object Oriented Programming
- * Goal: Create an interface for emulating a mechanical rotor in the Enigma program
- * Version 0.0.1 - 9/22/17
- * 		   0.0.2 - 9/23/17	Refactored to make this an extension of Ecryptor
- * 		   0.0.3 - 9/24/17  Added the getter for instance variable notch
- */
-
 package interfaces;
 
 public interface RotaryEncryptor extends Encryptor {
